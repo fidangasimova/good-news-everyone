1e198e6162324a443367dfa1c5ca1b110f67c778
fidangasimova <fidangasimova153@gmail.com>
2016-02-04 21:11:49 +0100

    Create a module

--- nofunctions.py
+++
@@ -1,8 +0,0 @@
-import feedparser
-feed = feedparser.parse('http://www.theguardian.com/world/rss')
-NEGATIVE_WORDS = ['war', 'war']
-for entry in feed['entries']:
-    title = entry['title']
-    for word in NEGATIVE_WORDS:
-        if word in title:
-            print title
