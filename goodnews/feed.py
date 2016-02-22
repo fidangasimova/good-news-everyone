794458f1aec6a220dfe9e98ffc597b6de8dce20f
fidangasimova <fidangasimova153@gmail.com>
2016-02-22 19:36:44 +0100

    Create a function to generate RSS feed

--- goodnews/feed.py
+++ goodnews/feed.py
@@ -1,20 +1,21 @@
 import feedparser
 import config
+import PyRSS2Gen
  
 def download_feed(url):
     return feedparser.parse(url)
 
-def filter_feed(feed):
+def filter_feed(feed_dict):
     positive_entries = []
-    for entry in feed['entries']:
-        title = entry['title']
+    for entry in feed_dict.entries:
+        title = entry.title
         if contains_negative_words(title):
             print title
         else:
             positive_entries.append(entry)
-    feed['entries'] = positive_entries
+    feed_dict.entries = positive_entries
 
-    return feed
+    return feed_dict
 
 def contains_negative_words(text):
     words = set()
@@ -28,4 +29,24 @@ def contains_negative_words(text):
 
     return False
           
-  
+def generate_rss(feed_dict):
+    items = []
+    for entry in feed_dict.entries:
+        item = PyRSS2Gen.RSSItem(
+            title = entry.title,
+            link = entry.link, 
+            description = entry.description,
+            pubDate = entry.published,
+        )
+        items.append(item)
+
+    rss = PyRSS2Gen.RSS2(
+        title = feed_dict.feed.title,
+        link = feed_dict.feed.link,
+        description = feed_dict.feed.description,
+        lastBuildDate = feed_dict.feed.published, 
+        items = items
+    )
+    return rss.to_xml()
+
+
